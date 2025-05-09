5b249ec5dc3e21ca283730d71bd6fbc68db44c03
mrzzrm <mrzzrm27@gmail.com>
2025-05-09 11:39:17 +0330

    creating plugin boilerplate

--- radmantaskmrz.php
+++ radmantaskmrz.php
@@ -1,1 +1,15 @@
 <?php
+/**
+ * Plugin Name: RadmanTaskMrz
+ * Plugin URI: https://mohammad-res.vercel.app/
+ * Description: A custom plugin for RadmanTaskMrz.
+ * Version: 1.0
+ * Author: Your Name
+ * Author URI: https://mohammad-res.vercel.app/
+ * License: GPL2
+ */
+
+// Avoid direct access
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
