ddb15e15df3caf9b08df970ec76b9a0f02d8e4a8
mrzzrm <mrzzrm27@gmail.com>
2025-05-09 13:13:03 +0330

    separate db creation 2

--- radmantaskmrz.php
+++ radmantaskmrz.php
@@ -65,28 +65,3 @@ function radmantaskmrz_settings_page() {
     echo '</div>';
 }
 
-
-// Function to create the database table when the plugin is activated
- function radmantaskmrz_create_db_table() {
-	global $wpdb;
-
-	// Define the global variable for table name
-	$table_name = $wpdb->prefix . 'radmantaskmrz_logs'; // Table name with WordPress prefix
-
-	// SQL query to create the logs table
-	$charset_collate = $wpdb->get_charset_collate();
-
-	$sql = "CREATE TABLE $table_name (
-            id BIGINT(20) NOT NULL AUTO_INCREMENT,
-            request_date DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-            response_content TEXT NOT NULL,
-            request_method VARCHAR(10) NOT NULL,
-            PRIMARY KEY  (id)
-        ) $charset_collate;";
-
-	// Include the WordPress database upgrade function
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-
-	// Create the table or update it if necessary
-	dbDelta( $sql );
-}
