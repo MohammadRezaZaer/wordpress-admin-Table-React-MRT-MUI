bd79210b2bdb59c60e367ae32d083ac98405c825
mrzzrm <mrzzrm27@gmail.com>
2025-05-13 01:02:05 +0330

    some old file cleaning

--- react-app2/src/index.tsx
+++ react-app2/src/index.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.css';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import {ProductTableContainer} from "./components/RequestLogsTable";
+import {QueryClient, QueryClientProvider} from 'react-query';
 import {InputForm} from "./components/url-save-form";
 import LogsTableWithReactQueryProvider from "./components/LogsTableMrt";
 
 const queryClient = new QueryClient();
 ReactDOM.render(
-
     <QueryClientProvider client={queryClient}>
 
         <InputForm/>
-<LogsTableWithReactQueryProvider/>
-        {/*<ProductTableContainer />*/}
+        <LogsTableWithReactQueryProvider/>
 
     </QueryClientProvider>
 
